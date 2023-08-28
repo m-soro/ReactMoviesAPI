8a24054e1ca52f401226e4269d078aff62c97251
Mark Soro <napoleon_soro@rocketmail.com>
2023-08-28 14:28:27 -0400

    added try catch in async function

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,20 @@ export default function App() {
 
   //Function to getMovies
   const getMovie = async (searchTerm) => {
-    // make fetch request and store response
-    const response = await fetch(
-      `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
-    );
-    // Parse JSON response into a javascript object
-    const data = await response.json();
-    //set the Movie state to the movie
-    setMovie(data);
+    try {
+      // make fetch request and store response
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      // Parse JSON response into a javascript object
+      const data = await response.json();
+      //set the Movie state to the movie
+      setMovie(data);
+      console.log(movie);
+    } catch (e) {
+      console.error(e);
+    }
   };
-  console.log(movie);
 
   useEffect(() => {
     getMovie("White Chicks");
